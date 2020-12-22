f184dc37564b52a28be7c66761b355a5e116b371
raininboat <74845844+raininboat@users.noreply.github.com>
2020-12-22 14:03:06 -0600

    Add files via upload

--- setRaRules.lua
+++ setRaRules.lua
@@ -12,11 +12,11 @@
 ]]
 
 command = {}
-command["(\\.|。|＊|\\*)(setrule)"] = "testing"
+command["(\\.|。|＊|\\*)(tra)\\s*(\\d+)\\s*(\\d+)"] = "testing"
 command["(\\.|。|＊|\\*)(setrule)\\s*?(\\d)"] = "setRuleClassic"
-command["(\\.|。|＊|\\*)(setrule)\\s*(\\d+\\.?\\d*)\\s(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)\\s*(\\d+\\.?\\d*)"] = "setRuleFull"
-local basic_path = dice.DiceDir()
-package.path = package.path..";"..dice.DiceDir().. "\\plugin\\?\\?.lua"
+command["(\\.|。|＊|\\*)(setrule)\\s*(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)\\s+(\\d+\\.?\\d*)"] = "setRuleFull"
+-- local basic_path = dice.DiceDir()
+-- package.path = package.path..";"..dice.DiceDir().. "\\plugin\\?\\?.lua"
 --require(Rainy)
 function setRuleClassic(msg)
     require("Rainy")
@@ -31,16 +31,41 @@ function setRuleFull(msg)
     local target = msg.tergetId     -- 获取窗口号
     --local userPath = basic_path
 --    dice.send(M.printstr(msg),target,1)
-    for i = 1, 9 do
+    for i = 1, tonumber(msg.str_max-1) do
         -- statements
-        setcoc[i] = msg.str[i+2]
+        -- if not tonumber(msg.str[i+2]) then return msg.str[i+2].."输入内容非法" end
+        setcoc[i] = tonumber(msg.str[i+2])
     end
+    if ((setcoc[1]+setcoc[2]*setcoc[9])>=(setcoc[5]+setcoc[6]*setcoc[9])) or ((setcoc[3]+setcoc[4]*setcoc[9])>=(setcoc[7]+setcoc[8]*setcoc[9]))
+    then
+        return "设置失败，当前大成功最大值大于大失败最小值"
+    end
+
     local userdata = M.getUserState(target,isGroup)
     userdata.setcoc = setcoc
     M.saveUserState(userdata,target,isGroup)
-    return
+    return M.tabletostring(userdata.setcoc)
     --if tonumber(msg.str_max==)
 end
+
 function testing(msg)
-    return "testing123\n"--..package.path.."\n"..dice.DiceDir()
+    require("Rainy")
+    local skill = tonumber(msg.str[3])
+    local val = tonumber(msg.str[4])
+    local isGroup = msg.msgType     -- 判断聊天类型
+    local target = msg.tergetId     -- 获取窗口号
+    local userdata = M.getUserState(target,isGroup)
+    local rank = M.Rasuccess(skill,val,userdata.setcoc)
+    local level = ""
+    if rank == 1 then level = "大成功"
+    elseif rank == 2 then level = "极难成功"
+    elseif rank == 3 then level = "困难成功"
+    elseif rank == 4 then level = "成功"
+    elseif rank == 5 then level = "失败"
+    elseif rank == 6 then level = "大失败"
+    else level = "【无法判定成功率】"
+    end
+    local resp = "test\n"..skill.." / "..val.." ["..level.."] \n"
+    -- resp = resp .. M.tabletostring(userdata.setcoc)
+    return resp  --..package.path.."\n"..dice.DiceDir()
 end
