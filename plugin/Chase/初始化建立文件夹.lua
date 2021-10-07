91ccdafe86f27f4f1f9e75349ea9ffb39a90e089
RainyZhou <74845844+raininboat@users.noreply.github.com>
2021-10-07 23:27:50 +0800

    Delete plugin directory

--- plugin/Chase/初始化建立文件夹.lua
+++
@@ -1,9 +0,0 @@
-function mkDirs(path)
-    os.execute('mkdir "' .. path .. '"')
-end
-
-basic_path = dice.DiceDir()
-mkDirs(basic_path.."\\RainyData\\group") -- 初始化存档路径
-mkDirs(basic_path.."\\RainyData\\QQ") -- 初始化存档路径
-mkDirs(basic_path.."\\RainyData\\story") -- 初始化存档路径
-mkDirs(basic_path.."\\RainyData\\storydata") -- 初始化存档路径
--- plugin/lib/others/初始化建立文件夹.lua
+++
@@ -1,6 +0,0 @@
-function mkDirs(path)
-    os.execute('mkdir "' .. path .. '"')
-end
-
-basic_path = dice.DiceDir() .. "\\user\\Rainy_plugin\\groups\\" -- 这里设置一下存档的初始地址
-mkDirs(basic_path) -- 初始化存档路径
